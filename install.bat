c1aabab8324dc402cda49256beab9747a87a908f
Matthew Taylor <hacks420chickens@outlook.com>
2021-07-11 21:37:33 +0100

    Fixed install.bat
    It was erroring because of a missing directory, so I just made it create the directory in the script.

--- install.bat
+++ install.bat
@@ -1,10 +1,12 @@
 @echo off
 echo Installing MCMap-Tool
 CMD /C npm i --loglevel error
-
-echo Downloading Map Colour Sets...
 CMD /C py -m pip install -r scripts/requirements.txt
+
+echo Downloading Map Colour Sets
+mkdir shared
 py scripts/mc_download_colours.py shared/colour_sets.json
+
 echo Finished Downloading!
 
 echo Thank you for choosing MCMap-Tool!
